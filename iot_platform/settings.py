79c1057004005349de5d749257b28dce4f40b2d2
Ubuntu <ubuntu@ip-172-26-0-161.ap-south-1.compute.internal>
2025-09-16 10:33:03 +0000

    Second Commit

--- iot_platform/settings.py
+++ iot_platform/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -61,4 +62,5 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
